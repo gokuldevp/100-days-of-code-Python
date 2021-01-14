8def1ecbab2618a4092a62b0f75f355c659b7469
gokuldevp <75772147+gokuldevp@users.noreply.github.com>
2021-01-14 10:46:32 +0530

    Day 1 of 100 days of python coding

---
+++ Day 1/program Day 1/day1 - Project.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Hi There! Welcome to Brand Name Generator \n")
+#2. Ask the user for the city that they grew up in.
+city = input("What is the Name of the City you Born in? \n")
+#3. Ask the user for the name of a pet.
+pet = input("\nWhat is the Name of your Pet? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("\nOur recommended brand name is : \n" + city.upper() + " " + pet.upper())
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
---
+++ Day 1/program Day 1/day1-input.py
@@ -0,0 +1,3 @@
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
